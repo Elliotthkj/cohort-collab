6d957d617e432485af0f436079ef23ee6ca0f13f
Elliott Johnson <elliotthkj1991@gmail.com>
2023-09-14 15:15:01 -0700

    Initial commit, implemented clerk login/logout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+
+export const metadata = {
+  title: 'CohortCollab',
+  description: 'Milestone 3 MERN project for UNLV bootcamp w/ Nextjs',
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
